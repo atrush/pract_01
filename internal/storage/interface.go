3fe6dca81f140af560f1c8dbd7f2ef9073720747
atrush <anton.trush@gmail.com>
2022-01-11 21:34:57 +0300

    refact rm URLStorage struct over storage interface

--- internal/storage/interface.go
+++ internal/storage/interface.go
@@ -1,15 +1,6 @@
 package storage
 
-type URLStorage struct {
-	URLStorage URLStorer
-}
 type URLStorer interface {
 	GetURL(shortURL string) (string, error)
 	SaveURL(srcURL string) (string, error)
-}
-
-func NewURLStorage(db URLStorer) *URLStorage {
-	return &URLStorage{
-		URLStorage: db,
-	}
 }
