07610774a4b1a459d52699619e9f3b8a743059f3
atrush <anton.trush@gmail.com>
2022-01-22 19:28:47 +0300

    refact handler tests

--- internal/api/handler_test.go
+++ internal/api/handler_test.go
@@ -16,74 +16,77 @@ import (
 func TestHandler_SaveURLHandler(t *testing.T) {
 
 	tests := []struct {
-		name     string
-		body     string
-		wantCode int
+		name        string
+		method      string
+		url         string
+		body        string
+		contentType string
+
+		outBodyExpected     string
+		outContTypeExpected string
+		outCodeExpected     int
+
+		initFixtures func(storage *inmemory.MapStorage)
 	}{
 		{
-			name:     "POST empty URL",
-			body:     "",
-			wantCode: 400,
+			name:            "POST empty URL",
+			method:          http.MethodPost,
+			url:             "/",
+			body:            "",
+			outCodeExpected: 400,
 		},
 		{
-			name:     "POST URL",
-			body:     "https://practicum.yandex.ru/",
-			wantCode: 201,
+			name:            "POST URL",
+			method:          http.MethodPost,
+			url:             "/",
+			body:            "https://practicum.yandex.ru/",
+			outCodeExpected: 201,
+		},
+		{
+			name:            "POST exist URL",
+			method:          http.MethodPost,
+			url:             "/",
+			body:            "https://practicum.yandex.ru/",
+			outCodeExpected: 201,
+			initFixtures: func(storage *inmemory.MapStorage) {
+				storage.SaveURL("wdwfff", "https://practicum.yandex.ru/")
+			},
+		},
+		{
+			name:            "GET not exist URL",
+			method:          http.MethodGet,
+			url:             "/aaaaaa",
+			outCodeExpected: 404,
+		},
+		{
+			name:            "GET exist URL",
+			method:          http.MethodGet,
+			url:             "/aaaaaa",
+			outCodeExpected: 307,
+			initFixtures: func(storage *inmemory.MapStorage) {
+				storage.SaveURL("aaaaaa", "https://practicum.yandex.ru/")
+			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			db := inmemory.NewStorage()
-			svc, _ := service.NewShortURLService(db)
-			handler := Handler{svc: svc}
-			r := NewRouter(handler)
+			if tt.initFixtures != nil {
+				tt.initFixtures(db)
+			}
 
-			request := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer([]byte(tt.body)))
+			svc, _ := service.NewShortURLService(db)
+			h := &Handler{svc: svc}
+			r := NewRouter(*h)
+
+			request := httptest.NewRequest(tt.method, tt.url, bytes.NewBuffer([]byte(tt.body)))
 			w := httptest.NewRecorder()
 			r.ServeHTTP(w, request)
 
 			res := w.Result()
 			defer res.Body.Close()
-			assert.True(t, res.StatusCode == tt.wantCode, "Ожидался код ответа %d, получен %d", tt.wantCode, w.Code)
-		})
-	}
-}
-
-func TestHandler_GetURLHandler(t *testing.T) {
-	// создаём массив тестов: имя и желаемый результат
-	tests := []struct {
-		name     string
-		request  string
-		wantCode int
-	}{
-		{
-			name:     "GET not exist URL with short URL",
-			request:  "/aaaaaa",
-			wantCode: 404,
-		},
-		{
-			name:     "GET not exist URL",
-			request:  "/aaaaaa/ddddd",
-			wantCode: 404,
-		},
-	}
-
-	for _, tt := range tests {
-		// запускаем каждый тест
-		t.Run(tt.name, func(t *testing.T) {
-			db := inmemory.NewStorage()
-			svc, _ := service.NewShortURLService(db)
-			handler := Handler{svc: svc}
-			r := NewRouter(handler)
-
-			request := httptest.NewRequest(http.MethodGet, tt.request, nil)
-			w := httptest.NewRecorder()
-			r.ServeHTTP(w, request)
-			res := w.Result()
-			defer res.Body.Close()
-
-			assert.True(t, res.StatusCode == tt.wantCode, "Ожидался код ответа %d, получен %d", tt.wantCode, w.Code)
+			assert.True(t, tt.outCodeExpected == 0 || res.StatusCode == tt.outCodeExpected, "Ожидался код ответа %d, получен %d", tt.outCodeExpected, w.Code)
 		})
 	}
 }
@@ -106,6 +109,7 @@ func Test_testSaveAndGetURL(t *testing.T) {
 	resBody, err := io.ReadAll(res.Body)
 	if err != nil {
 		t.Fatal(err)
+
 	}
 
 	shortURL := string(resBody)
