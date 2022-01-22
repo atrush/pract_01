3532be548b9154538ab7d532efad86dcbad0c4d5
atrush <anton.trush@gmail.com>
2022-01-22 21:54:35 +0300

    add api/shorten - SaveURLJSONHandler and tests

--- internal/api/router.go
+++ internal/api/router.go
@@ -8,6 +8,6 @@ func NewRouter(handler Handler) *chi.Mux {
 	r := chi.NewRouter()
 	r.Get("/{shortID}", handler.GetURLHandler)
 	r.Post("/", handler.SaveURLHandler)
-
+	r.Post("/api/shorten", handler.SaveURLJSONHandler)
 	return r
 }
