e86f5dd13c8245f399259d0f614e5fec14dd4f60
atrush <anton.trush@gmail.com>
2022-02-02 19:18:40 +0300

    add chi compress

--- internal/api/router.go
+++ internal/api/router.go
@@ -2,12 +2,16 @@ package api
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func NewRouter(handler Handler) *chi.Mux {
 	r := chi.NewRouter()
+	compress := middleware.NewCompressor(5, "/*")
+	r.Use(compress.Handler)
 	r.Get("/{shortID}", handler.GetURLHandler)
 	r.Post("/", handler.SaveURLHandler)
 	r.Post("/api/shorten", handler.SaveURLJSONHandler)
+
 	return r
 }
