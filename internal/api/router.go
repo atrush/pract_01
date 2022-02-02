0b435882f31ba426a9d571e77eb4b9c21fcd9a7b
atrush <anton.trush@gmail.com>
2022-02-02 19:08:22 +0300

    add chi gzip

--- internal/api/router.go
+++ internal/api/router.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func NewRouter(handler Handler) *chi.Mux {
@@ -9,5 +10,9 @@ func NewRouter(handler Handler) *chi.Mux {
 	r.Get("/{shortID}", handler.GetURLHandler)
 	r.Post("/", handler.SaveURLHandler)
 	r.Post("/api/shorten", handler.SaveURLJSONHandler)
+
+	compressor := middleware.NewCompressor(5, "/*")
+	r.Use(compressor.Handler)
+
 	return r
 }
