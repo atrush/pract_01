f3c7a0cdddea7ad8f164c3cb1d2591376f35a728
atrush <anton.trush@gmail.com>
2022-04-03 22:51:40 +0300

    add config from file, mod upd go v.1.18

--- pkg/config.go
+++ pkg/config.go
@@ -1,21 +1,25 @@
 package pkg
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-
 	"github.com/caarlos0/env/v6"
 	"github.com/go-playground/validator/v10"
+	"golang.org/x/exp/slices"
+	"os"
 )
 
 //  Config stores server config params.
 type Config struct {
-	ServerPort      string `env:"SERVER_ADDRESS" validate:"required,hostname_port"`
-	BaseURL         string `env:"BASE_URL" validate:"required,url"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH" validate:"-"`
-	DatabaseDSN     string `env:"DATABASE_DSN" validate:"-"`
-	Debug           bool   `env:"SHORTENER_DEBUG" envDefault:"false" validate:"-"`
-	EnableHTTPS     bool   `env:"ENABLE_HTTPS" envDefault:"false" validate:"-"`
+	ServerPort      string `env:"SERVER_ADDRESS" json:"server_address"  validate:"required,hostname_port"`
+	BaseURL         string `env:"BASE_URL" json:"base_url" validate:"required,url"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH" json:"file_storage_path"  validate:"-"`
+	DatabaseDSN     string `env:"DATABASE_DSN" json:"database_dsn"  validate:"-"`
+	EnableHTTPS     bool   `env:"ENABLE_HTTPS" json:"enable_https" envDefault:"false" validate:"-"`
+
+	Debug      bool   `env:"SHORTENER_DEBUG" json:"-" envDefault:"false" validate:"-"`
+	ConfigPath string `env:"CONFIG" json:"-" validate:"-"`
 }
 
 //  Default config params.
@@ -32,6 +36,7 @@ const (
 //  Reads flag params over default params, then redefines  with environment params.
 func NewConfig() (*Config, error) {
 	cfg := Config{}
+	cfg.tryReadFileConfig()
 	cfg.readFlagConfig()
 	if err := cfg.readEnvConfig(); err != nil {
 		return nil, err
@@ -61,7 +66,32 @@ func (c *Config) readFlagConfig() {
 	flag.StringVar(&c.DatabaseDSN, "d", defDatabaseDSN, "строка с адресом подключения к БД")
 	flag.BoolVar(&c.Debug, "debug", defDebug, "режим отладки")
 	flag.BoolVar(&c.EnableHTTPS, "s", defEnableHTTPS, "включения HTTPS в веб-сервере")
+	flag.BoolVar(&c.EnableHTTPS, "c", defEnableHTTPS, "файл конфигурации")
 	flag.Parse()
+}
+
+//  redefineConfig redefines config with new config.
+//  if string not empty override
+//  if bool true override
+func (c *Config) redefineConfig(nc *Config) {
+	if nc.BaseURL != "" {
+		c.BaseURL = nc.BaseURL
+	}
+	if nc.ServerPort != "" {
+		c.ServerPort = nc.ServerPort
+	}
+	if nc.FileStoragePath != "" {
+		c.FileStoragePath = nc.FileStoragePath
+	}
+	if nc.DatabaseDSN != "" {
+		c.DatabaseDSN = nc.DatabaseDSN
+	}
+	if nc.Debug {
+		c.Debug = nc.Debug
+	}
+	if nc.EnableHTTPS {
+		c.EnableHTTPS = nc.EnableHTTPS
+	}
 }
 
 //  readEnvConfig redefines config params with environment params.
@@ -72,23 +102,51 @@ func (c *Config) readEnvConfig() error {
 		return fmt.Errorf("ошибка чтения переменных окружения:%w", err)
 	}
 
-	if envConfig.BaseURL != "" {
-		c.BaseURL = envConfig.BaseURL
-	}
-	if envConfig.ServerPort != "" {
-		c.ServerPort = envConfig.ServerPort
-	}
-	if envConfig.FileStoragePath != "" {
-		c.FileStoragePath = envConfig.FileStoragePath
-	}
-	if envConfig.DatabaseDSN != "" {
-		c.DatabaseDSN = envConfig.DatabaseDSN
-	}
-	if envConfig.Debug {
-		c.Debug = envConfig.Debug
-	}
-	if envConfig.EnableHTTPS {
-		c.EnableHTTPS = envConfig.EnableHTTPS
-	}
+	c.redefineConfig(envConfig)
 	return nil
+}
+
+//  tryReadFromFile if exist flag -c or env CONFIG tries to read config from path.
+//  If read ok, overrides config with new config
+func (c *Config) tryReadFileConfig() {
+	path := getConfigPath()
+	if len(path) == 0 {
+		return
+	}
+
+	fileConfig, err := readConfigFromFile(path)
+	if err != nil {
+		return
+	}
+
+	c.redefineConfig(fileConfig)
+}
+
+func getConfigPath() string {
+	path := ""
+
+	// tries to get config path from env
+	path = os.Getenv("CONFIG")
+
+	//  tries to get config path from flags
+	args := os.Args
+	flagIndex := slices.Index(args, "-c")
+	if flagIndex > 0 && len(args) >= flagIndex+1 {
+		path = args[flagIndex+1]
+	}
+	return path
+}
+
+func readConfigFromFile(path string) (*Config, error) {
+	file, err := os.OpenFile(path, os.O_RDONLY, 0777)
+	if err != nil {
+		return nil, fmt.Errorf("error reading config from file: %w", err)
+	}
+	defer file.Close()
+
+	var c Config // целевой объект
+	if err := json.NewDecoder(file).Decode(&c); err != nil {
+		return nil, fmt.Errorf("error reading config from file: %w", err)
+	}
+	return &c, nil
 }
